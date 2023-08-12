d7da5b752516df43ca7ca127230737392e9ed55c
Bruno Ribaric <bruno@ribarich.me>
2023-08-12 12:40:39 +0200

    Progress

---
+++ local_packages/test_package/Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,33 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'My content element title',
+        'testpackage_newcontentelement',
+        'content-text'
+    ],
+    'textmedia',
+    'after'
+);
+
+$GLOBALS['TCA']['tt_content']['types']['testpackage_newcontentelement'] = [
+    'showitem' => '
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+               --palette--;;general,
+               header; Internal title (not displayed),
+               bodytext;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:bodytext_formlabel,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+               --palette--;;hidden,
+               --palette--;;access,
+         ',
+    'columnsOverrides' => [
+        'bodytext' => [
+            'config' => [
+                'enableRichtext' => true,
+                'richtextConfiguration' => 'default',
+            ],
+        ],
+    ],
+];
